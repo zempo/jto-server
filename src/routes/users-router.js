3d4a74e5e48d8999abceccece4d32c16f37412b7
zempo <zelenkosolomon@gmail.com>
2019-07-10 15:03:53 -0700

    add user fields validation

--- src/routes/users-router.js
+++ src/routes/users-router.js
@@ -11,9 +11,44 @@ usersRouter
   .get((req, res, next) => {
     // for profile creation?
     // get a particular user
+    // require authentication --> while logged-in, user will be displayed "logged in as John Doe" or "Welcome, John Doe"
   })
-  .post((req, res, next) => {
-    res.send("post here");
+  .post(jsonBodyParser, (req, res, next) => {
+    const { password, user_name, full_name, email } = req.body;
+    const newUser = { password, user_name, full_name, email };
+
+    async function validateUser(user, service) {
+      try {
+        // if we get don't get a null for error, we step out of function
+        let allKeysError = await service.checkAllFields(user);
+        if (allKeysError) return res.status(400).json({ error: allKeysError });
+
+        let emailError = await service.validateEmail(user.email);
+        if (emailError) return res.status(400).json({ error: emailError });
+
+        let passwordError = await service.validatePassword(user.password);
+        if (passwordError) return res.status(400).json({ error: passwordError });
+
+        let userNameExists = await service.uniqueUserName(req.app.get("db"), user.user_name);
+        console.log(user.email);
+        if (userNameExists) return res.status(400).json({ error: "Username already taken." });
+
+        let emailExists = await service.uniqueEmail(req.app.get("db"), user.email);
+        if (emailExists) return res.status(400).json({ error: "An account with this email has already been created." });
+        // else passing, return a response
+        return res.send("user passes validation");
+      } catch (error) {
+        next(error);
+      }
+    }
+
+    const result = validateUser(newUser, UsersService);
+    result;
+    // const emailError = UsersService.validateEmail(email);
+    // if (emailError) return res.status(400).json({ error: emailError });
+
+    // const passwordError = UsersService.validatePassword(password);
+    // if (passwordError) return res.status(400).json({ error: passwordError });
   });
 
 module.exports = usersRouter;
--- src/services/users-service.js
+++ src/services/users-service.js
@@ -11,24 +11,56 @@ const UsersService = {
     return db("jto_users")
       .where({ user_name })
       .first()
-      .then((user) => !!user);
+      .then((user) => {
+        return !!user;
+      });
   },
   uniqueEmail(db, email) {
-    return db("thingful_users")
+    return db("jto_users")
       .where({ email })
       .first()
-      .then((user) => !!user);
+      .then((user) => {
+        return !!user;
+      });
   },
-  validateEmail(email) {},
-  validatePassword(password) {},
+  checkAllFields(user) {
+    for (const [key, value] of Object.entries(user)) {
+      if (value == null) {
+        return `Missing required '${key}' to create new user`;
+      }
+    }
+    // if loops through and finds all keys
+    return null;
+  },
+  validateEmail(email) {
+    if (validator.validate(email) == false) {
+      return "Invalid email";
+    }
+    return null;
+  },
+  validatePassword(password) {
+    if (password.length < 8) {
+      return "Password must be longer than 8 characters";
+    }
+    if (password.length > 72) {
+      return "Password must be less than 72 characters";
+    }
+    if (password.startsWith(" ") || password.endsWith(" ")) {
+      return "Password must not start or end with empty spaces";
+    }
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+      return "Password must contain 1 upper case, lower case, number and special character";
+    }
+    return null;
+  },
   hashPassword(password) {
     return bcrypt.hash(password, 12);
   },
   serializeUser(user) {
     return {
       id: user.id,
-      full_name: xss(user.full_name),
       user_name: xss(user.user_name),
+      full_name: xss(user.full_name),
       email: xss(user.email),
       date_created: new Date(user.date_created)
     };
