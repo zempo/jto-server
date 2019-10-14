5346d684265a9e80bb17d1e251dabda736454fb9
zempo <zelenkosolomon@gmail.com>
2019-10-14 13:27:15 -0700

    deploy server

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,6 +4,7 @@ module.exports = {
   migrationDirectory: "migrations",
   driver: "pg",
   connectionString: process.env.NODE_ENV === "test" ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+  ssl: !!process.env.SSL,
   host: process.env.MIGRATION_DB_HOST,
   port: process.env.MIGRATION_DB_PORT,
   database: process.env.MIGRATION_DB_NAME,
