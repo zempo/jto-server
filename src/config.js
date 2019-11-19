64d0a5aea55a6cf0108f3c724e4e4d7d7aebde27
zempo <zelenkosolomon@gmail.com>
2019-11-18 17:39:31 -0800

    s

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "test-jwt-secret",
-  JWT_EXPIRY: process.env.JWT_EXPIRY || "5m",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "200m",
   CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
